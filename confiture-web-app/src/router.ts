7a4338404560b0c364aad8930ca434a29ccd9702
Quentin Bellanger <hello@quentin-bellanger.com>
2022-07-20 13:37:03 +0200

    rename audit first step page

--- confiture-web-app/src/router.ts
+++ confiture-web-app/src/router.ts
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "./pages/HomePage.vue";
 import NotFoundPage from "./pages/NotFoundPage.vue";
-import NewAuditPage from "./pages/NewAuditPage.vue";
+import NewAuditStepOnePage from "./pages/NewAuditStepOnePage.vue";
+import NewAuditStepTwoPage from "./pages/NewAuditStepTwoPage.vue";
 import AuditPage from "./pages/AuditPage.vue";
 
 const router = createRouter({
@@ -13,9 +14,14 @@ const router = createRouter({
       component: HomePage,
     },
     {
-      path: "/audits/nouveau",
-      name: "new-audit",
-      component: NewAuditPage,
+      path: "/audits/nouveau/1",
+      name: "new-audit-step-one",
+      component: NewAuditStepOnePage,
+    },
+    {
+      path: "/audits/nouveau/2",
+      name: "new-audit-step-two",
+      component: NewAuditStepTwoPage,
     },
     {
       path: "/audits/:uniqueId",
